6d57f6a19a1fc1092d405cc7665660258547567c
Danny Fung <dannyfung97@gmail.com>
2019-02-21 10:18:25 -0800

    init

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function () {
 	initializePage();
 });
 
@@ -9,5 +9,10 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	ga("send", "event", 'like', 'click');
 }
